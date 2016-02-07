52f16c76266ec4f4f79c2ea025f5a5258b47cef6
MahmoudDolah <mahmouddolah1995@gmail.com>
2016-02-06 19:14:35 -0500

    set1-challenge4
    My solution for challenge 4.
    Instructions can be found here: http://cryptopals.com/sets/1/challenges/4/

---
+++ set1-challenge4.py
@@ -0,0 +1,24 @@
+#!/usr/env/bin python
+
+import re
+from challenge3 import scoreText
+from challenge3 import decipherSingleXor
+
+def findSingleXor(lines):
+    ans = []
+    for aLine in lines:                                              # Get Lines from File
+        strLine = re.sub('[\n]','', aLine).decode('hex')
+        aLine = bytearray(strLine)
+        nini = decipherSingleXor(aLine)                              # Use code from challenge 3 and obligatory Monty Python reference
+        ans.append(nini)
+    return (max(ans, key = lambda x: x[0]))                     # return one with highest score
+
+
+def main():
+    with open('ch4File.txt', 'r') as f:
+        lines = f.readlines()
+
+    ans = findSingleXor(lines)
+    print ans, "\n"
+
+main()
